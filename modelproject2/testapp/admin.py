658da9fb58052e04c7f1a323a40c2daee1b8c755
ITSVINAY10 <itsvinayvelagana7@gmail.com>
2024-12-18 15:25:01 +0530

    up to date

--- modelproject2/testapp/admin.py
+++ modelproject2/testapp/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from testapp.models import Employee
 # Register your models here.
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['id',git'eno','ename','esal','eaddr']
+    list_display = ['id','eno','ename','esal','eaddr']
 admin.site.register(Employee,EmployeeAdmin)
